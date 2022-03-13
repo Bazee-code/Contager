eebe3214feef7381f725a6d6c06026b892e06cd3
Bazee-code <eugeneobazee2@gmail.com>
2022-03-14 00:33:36 +0300

    Fix bug in provider

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,11 +1,29 @@
-import React, {createContext, useReducer} from 'react-native';
+import React, {createContext, useReducer} from 'react';
+import authReducer from './reducers/authReducer';
+import authInitialState from './initialState/authState';
+import contactsInitialState from './initialState/contactsState';
+import contactsReducer from './reducers/contactsReducer';
 
 const GlobalContext = createContext();
 
 const GlobalProvider = ({children}) => {
+  const [authState, authDispatch] = useReducer(authReducer, authInitialState);
+  const [contactsState, contactsDispatch] = useReducer(
+    contactsReducer,
+    contactsInitialState,
+  );
+
   return (
-    <GlobalContext.GlobalProvider value={[]}>
+    <GlobalContext.Provider
+      value={{
+        authState,
+        contactsState,
+        authDispatch,
+        contactsDispatch,
+      }}>
       {children}
-    </GlobalContext.GlobalProvider>
+    </GlobalContext.Provider>
   );
 };
+
+export default GlobalProvider;
