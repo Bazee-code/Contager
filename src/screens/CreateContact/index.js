8baa03bd0e07b477e821084bc134af2b0b2dab41
Bazee-code <eugeneobazee2@gmail.com>
2022-03-13 21:48:23 +0300

    Add screen components

---
+++ src/screens/CreateContact/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const CreateContact = () => {
+  return (
+    <View>
+      <Text>Create contact screen</Text>
+    </View>
+  );
+};
+
+export default CreateContact;
---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+export {default as Contacts} from './Contacts';
+export {default as ContactDetail} from './ContactDetail';
+export {default as CreateContact} from './CreateContact';
+export {default as Login} from './Login';
+export {default as Register} from './Register';
+export {default as Settings} from './Settings';
