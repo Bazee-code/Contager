b3cee42e32043aac6fa9ce87bf06c2b81b263afd
Bazee-code <eugeneobazee2@gmail.com>
2022-03-13 21:48:55 +0300

    Make screen components dynamic

--- src/navigation/HomeNavigator.js
+++ src/navigation/HomeNavigator.js
@@ -1,39 +1,7 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import * as ROUTES from '../constants/routeNames';
-
-const Contacts = () => {
-  return (
-    <View>
-      <Text>Contacts screen</Text>
-    </View>
-  );
-};
-
-const ContactDetail = () => {
-  return (
-    <View>
-      <Text>Contact detail screen</Text>
-    </View>
-  );
-};
-
-const CreateContact = () => {
-  return (
-    <View>
-      <Text>Create contact screen</Text>
-    </View>
-  );
-};
-
-const Settings = () => {
-  return (
-    <View>
-      <Text>Settings screen</Text>
-    </View>
-  );
-};
+import {Contacts, ContactDetail, CreateContact, Settings} from '../screens';
 
 const HomeNavigator = () => {
   const HomeStack = createStackNavigator();
