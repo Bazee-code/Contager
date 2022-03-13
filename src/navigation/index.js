19948eedfa32caaf9b18370e8e6439ceba80d000
Bazee-code <eugeneobazee2@gmail.com>
2022-03-14 00:41:21 +0300

    Render values based on global context state

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthNavigator from './AuthNavigator';
+import DrawerNavigator from './DrawerNavigator';
 import HomeNavigator from './HomeNavigator';
+import {GlobalContext} from '../context/Provider';
 
 const AppNavContainer = () => {
+  const {
+    authState: {isLoggedIn},
+  } = useContext(GlobalContext);
+
   return (
     <NavigationContainer>
-      {/* <AuthNavigator /> */}
-
-      <HomeNavigator />
+      {isLoggedIn ? <HomeNavigator /> : <AuthNavigator />}
+      {/* <DrawerNavigator /> */}
     </NavigationContainer>
   );
 };
