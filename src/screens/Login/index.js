78e55d1c7d9586457e617c256a7814c7f8837572
Bazee-code <eugeneobazee2@gmail.com>
2022-03-14 15:57:01 +0300

    Display user errors

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,31 +1,30 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
 import React, {useState} from 'react';
 import Container from '../../components/common/container';
 import Input from '../../components/common/input';
 
 const Login = () => {
-  const [text, onChangeText] = useState('Useless Text');
-  const [number, onChangeNumber] = useState(null);
+  const [value, onChangeText] = useState('');
 
   return (
     <Container>
       <Input
         label="Username"
-        style={styles.input}
         onChangeText={onChangeText}
-        value={text}
+        value={value}
+        iconPosition="right"
+        error={'This field is required'}
+      />
+
+      <Input
+        label="Password"
+        onChangeText={onChangeText}
+        value={value}
+        icon={<Text>HIDE</Text>}
+        iconPosition="right"
       />
     </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-});
 
 export default Login;
