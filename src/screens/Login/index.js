d3e82ed87f1b9f00722693267e9fd8415b4a8a46
Bazee-code <eugeneobazee2@gmail.com>
2022-03-14 15:00:44 +0300

    Render Input HoC component

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,12 +1,31 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import Container from '../../components/common/container';
+import Input from '../../components/common/input';
 
 const Login = () => {
+  const [text, onChangeText] = useState('Useless Text');
+  const [number, onChangeNumber] = useState(null);
+
   return (
-    <View>
-      <Text>Login screen</Text>
-    </View>
+    <Container>
+      <Input
+        label="Username"
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+    </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
 export default Login;
