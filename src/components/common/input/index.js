ad34e9c98bd58be71a7d83ef8eda4944311beedb
Bazee-code <eugeneobazee2@gmail.com>
2022-03-14 15:57:53 +0300

    Show error object

--- src/components/common/input/index.js
+++ src/components/common/input/index.js
@@ -2,12 +2,44 @@ import {View, Text, TextInput} from 'react-native';
 import React from 'react';
 
 import styles from './styles';
+import colors from '../../../assets/theme/colors';
 
-const Input = ({onChangeText, style, value, label, icon}) => {
+const Input = ({
+  onChangeText,
+  style,
+  value,
+  label,
+  icon,
+  iconPosition,
+  error,
+}) => {
+  const getFlexDirection = () => {
+    if (icon && iconPosition) {
+      if (iconPosition === 'left') {
+        return 'row';
+      } else if (iconPosition === 'right') {
+        return 'row-reverse';
+      }
+    }
+  };
+
+  const getBorderColor = () => {
+    if (error) {
+      return colors.danger;
+    } else {
+      return colors.grey;
+    }
+  };
+
   return (
     <View style={styles.inputContainer}>
       {label && <Text>{label}</Text>}
-      <View style={styles.wrapper}>
+      <View
+        style={[
+          styles.wrapper,
+          {flexDirection: getFlexDirection()},
+          {borderColor: getBorderColor()},
+        ]}>
         <View>{icon && icon}</View>
         <TextInput
           style={[styles.textInput, style]}
@@ -15,6 +47,7 @@ const Input = ({onChangeText, style, value, label, icon}) => {
           value={value}
         />
       </View>
+      {error && <Text style={styles.error}>{error}</Text>}
     </View>
   );
 };
