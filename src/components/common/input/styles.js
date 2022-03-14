574cbe8e7777d812ae2f45b3201f3448ff79f671
Bazee-code <eugeneobazee2@gmail.com>
2022-03-14 15:38:09 +0300

    Add styles to text input

--- src/components/common/input/styles.js
+++ src/components/common/input/styles.js
@@ -1,5 +1,19 @@
 import {StyleSheet} from 'react-native';
+import colors from '../../../assets/theme/colors';
 
 export default StyleSheet.create({
-  textInput: {},
+  inputContainer: {
+    paddingVertical: 12,
+  },
+  wrapper: {
+    height: 42,
+    borderColor: colors.grey,
+    borderWidth: 1,
+    borderRadius: 4,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textInput: {
+    flex: 1,
+  },
 });
