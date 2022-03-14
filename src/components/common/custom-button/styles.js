744589d8e54b2bcc0b2ea01e7809d1874419febe
Bazee-code <eugeneobazee2@gmail.com>
2022-03-14 17:13:30 +0300

    Style custom button element

---
+++ src/components/common/custom-button/styles.js
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+import colors from '../../../assets/theme/colors';
+
+export default StyleSheet.create({
+  inputContainer: {
+    paddingVertical: 12,
+  },
+  wrapper: {
+    height: 42,
+    marginVertical: 5,
+    paddingHorizontal: 5,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    marginTop: 5,
+  },
+  textInput: {
+    flex: 1,
+    width: '100%',
+  },
+  error: {
+    color: colors.danger,
+    paddingTop: 5,
+    fontSize: 12,
+  },
+});
