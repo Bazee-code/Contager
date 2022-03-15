61a13e9ca0bdc356ebfe6089f377d33e1cbd8aa7
Bazee-code <eugeneobazee2@gmail.com>
2022-03-15 12:02:06 +0300

    Render register login form

--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,12 +1,9 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import RegisterForm from '../../components/forms/register';
 
 const Register = () => {
-  return (
-    <View>
-      <Text>Register screen</Text>
-    </View>
-  );
+  return <RegisterForm />;
 };
 
 export default Register;
