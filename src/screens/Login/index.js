c81c1124db4e6089b6110ba54172d8af12b4724c
Bazee-code <eugeneobazee2@gmail.com>
2022-03-15 11:57:12 +0300

    Add register page navigator

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,30 +1,8 @@
-import {View, Text} from 'react-native';
-import React, {useState} from 'react';
-import Container from '../../components/common/container';
-import Input from '../../components/common/input';
+import React from 'react';
+import LoginForm from '../../components/forms/login';
 
 const Login = () => {
-  const [value, onChangeText] = useState('');
-
-  return (
-    <Container>
-      <Input
-        label="Username"
-        onChangeText={onChangeText}
-        value={value}
-        iconPosition="right"
-        error={'This field is required'}
-      />
-
-      <Input
-        label="Password"
-        onChangeText={onChangeText}
-        value={value}
-        icon={<Text>HIDE</Text>}
-        iconPosition="right"
-      />
-    </Container>
-  );
+  return <LoginForm />;
 };
 
 export default Login;
