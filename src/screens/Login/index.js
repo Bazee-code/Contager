3fca2b53b8fad99bf92aa333edfcc71eae30f39f
Bazee-code <eugeneobazee2@gmail.com>
2022-03-15 22:46:05 +0300

    Render header icon

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LoginForm from '../../components/forms/login';
 
 const Login = () => {
-  return <LoginForm />;
+  const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const onChange = ({name, value}) => {
+    setForm({...form, [name]: value});
+  };
+
+  const onSubmit = () => {
+    // validation
+    console.log('form=>', form);
+  };
+
+  return (
+    <LoginForm
+      form={form}
+      errors={errors}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
 };
 
 export default Login;
