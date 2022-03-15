ec8aa5821afe240073dde37619c6270cb0dbf5a1
Bazee-code <eugeneobazee2@gmail.com>
2022-03-15 23:25:14 +0300

    Add image icon styles

--- src/components/forms/login/styles.js
+++ src/components/forms/login/styles.js
@@ -3,17 +3,41 @@ import colors from '../../../assets/theme/colors';
 
 export default StyleSheet.create({
   loginHeaderSection: {},
-  loginHeaderImage: {},
+  loginHeaderImage: {
+    height: 150,
+    width: 150,
+    borderRadius: 100,
+    alignSelf: 'center',
+    marginBottom: 10,
+  },
   loginHeaderTitle: {
     textAlign: 'center',
     fontSize: 25,
-    fontWeight: '800',
+    fontWeight: '900',
+    color: '#000',
   },
   loginHeaderSubTitle: {
     textAlign: 'center',
     fontSize: 20,
     fontWeight: '600',
-    color: 'grey',
+    color: '#000',
   },
-  formSection: {},
+  formSection: {
+    marginTop: 10,
+  },
+  infoSection: {
+    flexDirection: 'row',
+    paddingTop: 10,
+  },
+  infoText: {
+    fontSize: 15,
+    fontWeight: '600',
+    paddingRight: 10,
+    color: '#000',
+  },
+  infoLink: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: colors.primary,
+  },
 });
