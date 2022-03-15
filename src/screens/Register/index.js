635e21fa1dbc2d5eb76f6942d70814feafe88741
Bazee-code <eugeneobazee2@gmail.com>
2022-03-15 23:41:23 +0300

    Add onChange & onSubmit event handlers & pass props

--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,9 +1,27 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import RegisterForm from '../../components/forms/register';
 
 const Register = () => {
-  return <RegisterForm />;
+  const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const onChange = ({value, name}) => {
+    setForm({...form, [name]: value});
+  };
+
+  const onSubmit = () => {
+    console.log('register form', form);
+  };
+
+  return (
+    <RegisterForm
+      form={form}
+      errors={errors}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
 };
 
 export default Register;
