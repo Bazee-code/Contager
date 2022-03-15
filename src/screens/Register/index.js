d7e6fd06e4592e85c182812dd5282382ed198d13
Bazee-code <eugeneobazee2@gmail.com>
2022-03-16 00:32:15 +0300

    Add password length validator

--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -8,6 +8,24 @@ const Register = () => {
 
   const onChange = ({value, name}) => {
     setForm({...form, [name]: value});
+
+    if (value !== '') {
+      if (name === 'password') {
+        if (value.length < 6) {
+          setErrors(prev => {
+            return {...prev, [name]: 'A minimum of 6 characters is required'};
+          });
+        } else {
+          setErrors(prev => {
+            return {...prev, [name]: null};
+          });
+        }
+      }
+    } else {
+      setErrors(prev => {
+        return {...prev, [name]: 'This field is required'};
+      });
+    }
   };
 
   const onSubmit = () => {
