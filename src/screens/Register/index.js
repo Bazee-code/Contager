adb30d9a71e9e1e36114b5e6161acf1125dc403e
Bazee-code <eugeneobazee2@gmail.com>
2022-03-16 00:00:32 +0300

    Create form input validations

--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -11,7 +11,32 @@ const Register = () => {
   };
 
   const onSubmit = () => {
-    console.log('register form', form);
+    if (!form.userName) {
+      setErrors(prev => {
+        return {...prev, userName: 'User name is required'};
+      });
+    }
+
+    if (!form.password) {
+      setErrors(prev => {
+        return {...prev, password: 'Password is required'};
+      });
+    }
+    if (!form.firstName) {
+      setErrors(prev => {
+        return {...prev, firstName: 'First Name is required'};
+      });
+    }
+    if (!form.lastName) {
+      setErrors(prev => {
+        return {...prev, lastName: 'Last Name is required'};
+      });
+    }
+    if (!form.email) {
+      setErrors(prev => {
+        return {...prev, email: 'Email is required'};
+      });
+    }
   };
 
   return (
