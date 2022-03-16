4af6f2a8b6df8e6c69f8940e8583485b591c32f7
Bazee-code <eugeneobazee2@gmail.com>
2022-03-17 00:47:48 +0300

    Code up the auth reducer cases

--- src/context/reducers/authReducer.js
+++ src/context/reducers/authReducer.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import {
+  REGISTER_ERRORS,
+  REGISTER_LOADING,
+  REGISTER_SUCCESS,
+} from '../action-types/authActionTypes';
 
 const authReducer = (state, action) => {
   const {type, payload} = action;
 
   switch (type) {
-    case 'LOGIN':
-      return {};
+    case REGISTER_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
 
-    default:
-      return state;
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        data: payload,
+        loading: false,
+      };
+
+    case REGISTER_ERRORS:
+      return {
+        ...state,
+        errors: payload,
+        loading: false,
+      };
   }
 };
 
