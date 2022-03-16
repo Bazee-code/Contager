a07c298ae770c72bc1fad645d063c643feec6c0d
Bazee-code <eugeneobazee2@gmail.com>
2022-03-16 20:32:37 +0300

    Fix config bug

--- src/helpers/axiosInterceptor.js
+++ src/helpers/axiosInterceptor.js
@@ -3,24 +3,24 @@ import env from '../config/env';
 
 let headers = {};
 
-const instance = axios.create({
+const axiosInstance = axios.create({
   baseURL: env.DEV_API_URL,
   headers,
 });
+console.log('url', axiosInstance.baseURL);
 
-instance.interceptors.request.use(
-  config => {
-    async config => {
-      const token = await AsyncStorage.getItem('token');
+axiosInstance.interceptors.request.use(
+  async config => {
+    const token = await AsyncStorage.getItem('token');
 
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-    };
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
   },
   error => {
     return Promise.reject(error);
   },
 );
 
-export default instance;
+export default axiosInstance;
