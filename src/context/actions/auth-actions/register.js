606e293d59842afbf3aef6c12e1d0fde5fd0bbd7
Bazee-code <eugeneobazee2@gmail.com>
2022-03-17 00:29:31 +0300

    Add loading state & catch errors callback

--- src/context/actions/auth-actions/register.js
+++ src/context/actions/auth-actions/register.js
@@ -1,28 +1,42 @@
 import axiosInstance from '../../../helpers/axiosInterceptor';
-import { REGISTER_LOADING } from '../../action-types/authActionTypes';
+import {
+  REGISTER_ERRORS,
+  REGISTER_LOADING,
+  REGISTER_SUCCESS,
+} from '../../action-types/authActionTypes';
 
 export default ({
-  email,
-  password,
-  username,
-  firstName: first_name,
-  lastName: last_name,
-}) => dispatch =>{
-    // loading
+    email,
+    password,
+    username,
+    firstName: first_name,
+    lastName: last_name,
+  }) =>
+  dispatch => {
     dispatch({
-        type: REGISTER_LOADING
-    })
-  axiosInstance
-    .post('/auth/register', {
-      email,
-      password,
-      username,
-      first_name,
-      last_name,
-    })
-    .then(res => res.data)
-    .then(data => {})
-    .catch(e => {
-      console.log(e);
+      type: REGISTER_LOADING,
     });
-};
+    axiosInstance
+      .post('/auth/register', {
+        email,
+        password,
+        username,
+        first_name,
+        last_name,
+      })
+      .then(res => res.data)
+      .then(data => {
+        dispatch({
+          type: REGISTER_SUCCESS,
+          payload: data,
+        });
+      })
+      .catch(e => {
+        dispatch({
+          type: REGISTER_ERRORS,
+          payload: e.response
+            ? e.response.data
+            : {error: 'Something went wrong!'},
+        });
+      });
+  };
