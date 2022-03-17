b1cd9c78674ea374e62a342e7bbf936a377ec66a
Bazee-code <eugeneobazee2@gmail.com>
2022-03-17 18:53:55 +0300

    Add activity indicator after text

--- src/components/common/custom-button/styles.js
+++ src/components/common/custom-button/styles.js
@@ -2,28 +2,29 @@ import {StyleSheet} from 'react-native';
 import colors from '../../../assets/theme/colors';
 
 export default StyleSheet.create({
-  inputContainer: {
-    paddingVertical: 12,
-  },
   wrapper: {
     height: 42,
-    marginVertical: 5,
+
     paddingHorizontal: 5,
+
+    marginVertical: 5,
     borderRadius: 4,
     alignItems: 'center',
     justifyContent: 'space-evenly',
-    marginTop: 5,
   },
+
+  loaderSection: {
+    flexDirection: 'row',
+  },
+
   textInput: {
     flex: 1,
     width: '100%',
   },
+
   error: {
     color: colors.danger,
-    paddingTop: 5,
+    paddingTop: 4,
     fontSize: 12,
-  },
-  loaderSection: {
-    flexDirection: 'row',
   },
 });
