b80630a333d908e3e1d8383136d1297d38b99e73
Bazee-code <eugeneobazee2@gmail.com>
2022-03-17 18:28:36 +0300

    Fix bug in export

--- src/config/env.js
+++ src/config/env.js
@@ -1,11 +1,11 @@
 import {DEV_API_URL, PROD_API_URL} from '@env';
 
 const devEnvVariables = {
-  DEV_API_URL,
+  BACKEND_URL: DEV_API_URL,
 };
 
 const prodEnvVariables = {
-  PROD_API_URL,
+  BACKEND_URL: PROD_API_URL,
 };
 
 export default __DEV__ ? devEnvVariables : prodEnvVariables;
