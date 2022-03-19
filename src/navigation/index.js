a8079b37e25a1a462405cbc01415fab72bc375f4
Bazee-code <eugeneobazee2@gmail.com>
2022-03-20 00:02:15 +0300

    Persist authenticated users

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,20 +1,49 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {ActivityIndicator} from 'react-native';
 import AuthNavigator from './AuthNavigator';
 import DrawerNavigator from './DrawerNavigator';
 import HomeNavigator from './HomeNavigator';
 import {GlobalContext} from '../context/Provider';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const AppNavContainer = () => {
   const {
     authState: {isLoggedIn},
   } = useContext(GlobalContext);
+
+  const [isAuth, setIsAuth] = useState(false);
+  const [authCompleted, setAuthCompleted] = useState(false);
+
+  const getUser = async () => {
+    try {
+      const user = await AsyncStorage.getItem('user');
+
+      if (user) {
+        setAuthDone(true);
+        setIsAuth(true);
+      } else {
+        setIsAuth(false);
+        setAuthDone(false);
+      }
+    } catch (e) {}
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   console.log('isloggedIn', isLoggedIn);
   return (
-    <NavigationContainer>
-      {isLoggedIn ? <HomeNavigator /> : <AuthNavigator />}
-      {/* <DrawerNavigator /> */}
-    </NavigationContainer>
+    <>
+      {authCompleted ? (
+        <NavigationContainer>
+          {isLoggedIn || isAuth ? <HomeNavigator /> : <AuthNavigator />}
+        </NavigationContainer>
+      ) : (
+        <ActivityIndicator />
+      )}
+    </>
   );
 };
 
