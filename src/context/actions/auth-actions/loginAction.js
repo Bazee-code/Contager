30e66df2eb2caf189b5a01ae1bfc2a7ac96c3a88
Bazee-code <eugeneobazee2@gmail.com>
2022-03-19 23:35:12 +0300

    Store user token & auth user in asyncstorage

--- src/context/actions/auth-actions/loginAction.js
+++ src/context/actions/auth-actions/loginAction.js
@@ -1,26 +1,40 @@
-import axios from 'axios';
-import {LOGIN_ERRORS, LOGIN_START, LOGIN_SUCCESS} from '../../action-types/authActionTypes';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axiosInstance from '../../../helpers/axiosInterceptor';
+import {
+  LOGIN_ERRORS,
+  LOGIN_START,
+  LOGIN_SUCCESS,
+} from '../../action-types/authActionTypes';
 
-const loginAction = (userData) => dispatch => {
-  dispatch({
-    type: LOGIN_START,
-  });
+const loginAction =
+  ({userName: username, password}) =>
+  dispatch => {
+    dispatch({
+      type: LOGIN_START,
+    });
 
-  axiosInstance 
-    .post(``,userData)
-    .then(res=>res.data)
-    .then((data)=>{
+    axiosInstance
+      .post(`/auth/login`, {
+        username,
+        password,
+      })
+      .then(res => res.data)
+      .then(data => {
+        AsyncStorage.setItem('token', res.data.token);
+        AsyncStorage.setItem('user', JSON.stringify(res.data.user));
         dispatch({
-            type : LOGIN_SUCCESS,
-            payload : data
-        })
-    })
-    .catch((e)=>{
+          type: LOGIN_SUCCESS,
+          payload: data,
+        });
+      })
+      .catch(e => {
         dispatch({
-            type: LOGIN_ERRORS,
-            payload : e.response ? e.response.data : {error : 'something went wrong!'}
-        })
-    })
-};
+          type: LOGIN_ERRORS,
+          payload: e.response
+            ? e.response.data
+            : {error: 'something went wrong!'},
+        });
+      });
+  };
 
 export default loginAction;
