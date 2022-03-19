5d0369e12b6519271c5baff0db66018ff7cd5c93
Bazee-code <eugeneobazee2@gmail.com>
2022-03-19 23:27:43 +0300

    Finish login user functionality

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -2,10 +2,11 @@ import React, {useState, useContext} from 'react';
 import LoginForm from '../../components/forms/login';
 import loginAction from '../../context/actions/auth-actions/loginAction';
 import {GlobalContext} from '../../context/Provider';
+import NetworkLogger from 'react-native-network-logger';
+import {ScrollView} from 'react-native-gesture-handler';
 
 const Login = () => {
   const [form, setForm] = useState({});
-  const [errors, setErrors] = useState({});
 
   const {
     authDispatch,
@@ -14,62 +15,26 @@ const Login = () => {
 
   const onChange = ({name, value}) => {
     setForm({...form, [name]: value});
-
-    if (value !== '') {
-      if (name === 'password') {
-        if (value.length < 6) {
-          setErrors(prev => {
-            return {...prev, [name]: 'This field needs min 6 characters'};
-          });
-        } else {
-          setErrors(prev => {
-            return {...prev, [name]: null};
-          });
-        }
-      } else {
-        setErrors(prev => {
-          return {...prev, [name]: null};
-        });
-      }
-    } else {
-      setErrors(prev => {
-        return {...prev, [name]: 'This field is required'};
-      });
-    }
   };
 
   const onSubmit = () => {
-    if (!form.userName) {
-      setErrors(prev => {
-        return {...prev, userName: 'User name is required'};
-      });
-    }
-
-    if (!form.password) {
-      setErrors(prev => {
-        return {...prev, password: 'Password is required'};
-      });
-    }
-
-    if (
-      Object.keys(form).length === 2 &&
-      Object.keys(form).every(item => item.trim().length > 0) &&
-      Object.keys(errors).every(item => !item)
-    ) {
-      // dispatch login action here
+    console.log('form', form);
+    if (form.userName && form.password) {
       loginAction(form)(authDispatch);
     }
   };
 
   return (
-    <LoginForm
-      form={form}
-      errors={errors}
-      onChange={onChange}
-      onSubmit={onSubmit}
-      error={error}
-      loading={loading}
-    />
+    <ScrollView>
+      <LoginForm
+        form={form}
+        onChange={onChange}
+        onSubmit={onSubmit}
+        error={error}
+        loading={loading}
+      />
+      <NetworkLogger />
+    </ScrollView>
   );
 };
 
