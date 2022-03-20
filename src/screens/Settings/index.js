ba2a0c863381df88a48fa34a6e246eb14884ca45
Bazee-code <eugeneobazee2@gmail.com>
2022-03-20 19:14:18 +0300

    Add confirm alert on handlelogout handler

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -1,11 +1,26 @@
-import {View, Text} from 'react-native';
+import {View, Text, Alert} from 'react-native';
 import React from 'react';
+import Container from '../../components/common/container';
+import Profile from '../Profile';
 
 const Settings = () => {
+  const handleLogout = () => {
+    console.log('clicked logout button');
+    Alert.alert('Logout!', 'Are you sure you want to logout? ', [
+      {
+        text: 'Cancel',
+        onPress: () => {},
+      },
+      {
+        text: 'OK',
+        onPress: () => {},
+      },
+    ]);
+  };
   return (
-    <View>
-      <Text>Settings screen</Text>
-    </View>
+    <Container>
+      <Profile handleLogout={handleLogout} />
+    </Container>
   );
 };
 
