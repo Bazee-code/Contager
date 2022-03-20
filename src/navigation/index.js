2271c518b99bf7e5db3d32536f5b0b6691ba55a3
Bazee-code <eugeneobazee2@gmail.com>
2022-03-20 20:15:57 +0300

    Switch out home navigator with bottom tab navigator

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,47 +2,39 @@ import React, {useContext, useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {ActivityIndicator} from 'react-native';
 import AuthNavigator from './AuthNavigator';
-import DrawerNavigator from './DrawerNavigator';
-import HomeNavigator from './HomeNavigator';
 import {GlobalContext} from '../context/Provider';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import BottomTabNavigator from './BottomTabNavigator';
 
 const AppNavContainer = () => {
   const {
     authState: {isLoggedIn},
   } = useContext(GlobalContext);
 
-  const [isAuth, setIsAuth] = useState(false);
-  const [authCompleted, setAuthCompleted] = useState(false);
+  const [isAuth, setIsAuth] = useState(isLoggedIn);
 
   const getUser = async () => {
     try {
       const user = await AsyncStorage.getItem('user');
 
       if (user) {
-        setAuthDone(true);
         setIsAuth(true);
       } else {
         setIsAuth(false);
-        setAuthDone(false);
       }
     } catch (e) {}
   };
 
   useEffect(() => {
     getUser();
-  }, []);
+  }, [isLoggedIn]);
 
   console.log('isloggedIn', isLoggedIn);
   return (
     <>
-      {authCompleted ? (
-        <NavigationContainer>
-          {isLoggedIn || isAuth ? <HomeNavigator /> : <AuthNavigator />}
-        </NavigationContainer>
-      ) : (
-        <ActivityIndicator />
-      )}
+      <NavigationContainer>
+        {isAuth ? <BottomTabNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
     </>
   );
 };
