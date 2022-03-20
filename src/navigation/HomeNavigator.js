e87a0a763d179aaad442fa71c17f979b1186b5e4
Bazee-code <eugeneobazee2@gmail.com>
2022-03-20 20:12:36 +0300

    Restructure rendered screens

--- src/navigation/HomeNavigator.js
+++ src/navigation/HomeNavigator.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import * as ROUTES from '../constants/routeNames';
-import {Contacts, ContactDetail, CreateContact, Settings} from '../screens';
+import {Contacts, ContactDetail} from '../screens';
 
 const HomeNavigator = () => {
   const HomeStack = createStackNavigator();
 
   return (
-    <HomeStack.Navigator initialRouteName={ROUTES.CONTACT_LIST}>
+    <HomeStack.Navigator>
       <HomeStack.Screen name={ROUTES.CONTACT_LIST} component={Contacts} />
       <HomeStack.Screen
         name={ROUTES.CONTACT_DETAIL}
         component={ContactDetail}
       />
-      <HomeStack.Screen
-        name={ROUTES.CREATE_CONTACT}
-        component={CreateContact}
-      />
-      <HomeStack.Screen name={ROUTES.SETTINGS} component={Settings} />
     </HomeStack.Navigator>
   );
 };
