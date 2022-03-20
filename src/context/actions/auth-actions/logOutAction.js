ea87a8002bc8fd43f64d55b926514503f872388b
Bazee-code <eugeneobazee2@gmail.com>
2022-03-20 20:09:52 +0300

    Create logout user action

---
+++ src/context/actions/auth-actions/logOutAction.js
@@ -0,0 +1,13 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {LOGOUT_USER} from '../../action-types/authActionTypes';
+
+const logoutAction = () => dispatch => {
+  AsyncStorage.removeItem('token');
+  AsyncStorage.removeItem('user');
+
+  dispatch({
+    type: LOGOUT_USER,
+  });
+};
+
+export default logoutAction;
