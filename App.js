b64979c14be3b00c4fab11915cbe1b18eaeea42f
Bazee-code <eugeneobazee2@gmail.com>
2022-03-20 20:18:37 +0300

    Remove console logs

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import AppNavContainer from './src/navigation';
-import GlobalProvider from './src/context/Provider';
+import {GlobalProvider} from './src/context/Provider';
 
 const App = () => {
   return (
     <GlobalProvider>
-      <AppNavContainer />;
+      <AppNavContainer />
     </GlobalProvider>
   );
 };
