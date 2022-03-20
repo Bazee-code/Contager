7abbaa316ae19566eaf8460bc483fc10c1f6b978
Bazee-code <eugeneobazee2@gmail.com>
2022-03-20 20:11:22 +0300

    Create logout user action type

--- src/context/action-types/authActionTypes.js
+++ src/context/action-types/authActionTypes.js
@@ -7,3 +7,5 @@ export const CLEAR_AUTH_STATE = 'CLEAR_AUTH_STATE';
 export const LOGIN_START = 'LOGIN_START';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_ERRORS = 'LOGIN_ERRORS';
+
+export const LOGOUT_USER = 'LOGOUT_USER';
