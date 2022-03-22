1681a70d4946d2b7f13fa8bf20fbac11f2c7042d
Bazee-code <eugeneobazee2@gmail.com>
2022-03-22 15:00:58 +0300

    Add useEffect to dispatch action on screen mount

--- src/components/contacts/index.js
+++ src/components/contacts/index.js
@@ -1,19 +1,25 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useContext, useEffect, useCallback} from 'react';
 import AppModal from '../common/modal';
-import CustomButton from '../common/custom-button';
+import {GlobalContext} from '../../context/Provider';
+import getContactAction from '../../context/actions/contact-actions/getContactAction';
 
 const ContactsComponent = ({modalVisible, setModalVisible}) => {
+  const {
+    contactsDispatch,
+    contactsState: {
+      getContacts: {data, loading},
+    },
+  } = useContext(GlobalContext);
+  console.log('data', data);
+
+  useEffect(() => {
+    getContactAction()(contactsDispatch);
+  }, [data]);
+
   return (
     <View>
       <AppModal modalVisible={modalVisible} setModalVisible={setModalVisible} />
-      <CustomButton
-        onPress={() => {
-          setModalVisible(true);
-        }}
-        secondary
-        title="Show modal"
-      />
     </View>
   );
 };
--- src/screens/Contacts/index.js
+++ src/screens/Contacts/index.js
@@ -1,18 +1,22 @@
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 import React, {useState} from 'react';
 import Container from '../../components/common/container';
 import ContactsComponent from '../../components/contacts';
+import NetworkLogger from 'react-native-network-logger';
 
 const Contact = () => {
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
-    <Container>
-      <ContactsComponent
-        modalVisible={modalVisible}
-        setModalVisible={setModalVisible}
-      />
-    </Container>
+    <ScrollView>
+      <Container>
+        <ContactsComponent
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+        />
+        <NetworkLogger />
+      </Container>
+    </ScrollView>
   );
 };
 
