450a9f9299597667f163dc495bd74e0d59c487bb
Bazee-code <eugeneobazee2@gmail.com>
2022-03-22 13:09:15 +0300

    Create getContactList action

---
+++ src/context/actions/contact-actions/getContactAction.js
@@ -0,0 +1,29 @@
+import {
+  GET_CONTACTS_ERRORS,
+  GET_CONTACTS_START,
+  GET_CONTACTS_SUCCESS,
+} from '../../action-types/contactsActionTypes';
+
+const getContactAction = () => dispatch => {
+  dispatch({
+    type: GET_CONTACTS_START,
+  });
+
+  axiosInstance
+    .get(``)
+    .then(res => res.data)
+    .then(data => {
+      dispatch({
+        type: GET_CONTACTS_SUCCESS,
+        payload: data,
+      });
+    })
+    .catch(e => {
+      dispatch({
+        type: GET_CONTACTS_ERRORS,
+        payload: e ? e.response.data : {error: 'Something went wrong'},
+      });
+    });
+};
+
+export default getContactAction;
