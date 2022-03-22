b28c69e2956ab9606e8d7309987a8a931f2b3353
Bazee-code <eugeneobazee2@gmail.com>
2022-03-22 12:53:05 +0300

    Add redirect callback

--- src/context/actions/auth-actions/logOutAction.js
+++ src/context/actions/auth-actions/logOutAction.js
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {LOGOUT_USER} from '../../action-types/authActionTypes';
 
-const logoutAction = () => dispatch => {
+const logoutAction = () => dispatch => () => {
   AsyncStorage.removeItem('token');
   AsyncStorage.removeItem('user');
 
