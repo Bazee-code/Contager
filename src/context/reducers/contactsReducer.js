6dec7edc23081ee6e4544e3617bb43894b23b13b
Bazee-code <eugeneobazee2@gmail.com>
2022-03-22 13:10:00 +0300

    Create getContactList action reducers

--- src/context/reducers/contactsReducer.js
+++ src/context/reducers/contactsReducer.js
@@ -1,9 +1,30 @@
-const contactsReducer = (state, action) => {
-  const {type, payload} = action;
+import {
+  GET_CONTACTS_ERRORS,
+  GET_CONTACTS_START,
+  GET_CONTACTS_SUCCESS,
+} from '../action-types/contactsActionTypes';
 
+const contactsReducer = (state, {type, action}) => {
   switch (type) {
-    case 'GET_CONTACTS':
-      return state;
+    case GET_CONTACTS_START:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case GET_CONTACTS_SUCCESS:
+      return {
+        ...state,
+        data: payload,
+        loading: false,
+      };
+
+    case GET_CONTACTS_ERRORS:
+      return {
+        ...state,
+        errors: payload,
+        loading: false,
+      };
 
     default:
       return state;
