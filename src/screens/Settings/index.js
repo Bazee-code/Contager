3d4eca7fc14d044ff0f05b637d19b58e79762934
Bazee-code <eugeneobazee2@gmail.com>
2022-03-22 12:54:07 +0300

    Redirect user to login page after successful logout

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -4,9 +4,12 @@ import Container from '../../components/common/container';
 import Profile from '../Profile';
 import {GlobalContext} from '../../context/Provider';
 import logoutAction from '../../context/actions/auth-actions/logOutAction';
+import {useNavigation} from '@react-navigation/native';
+import {LOGIN} from '../../constants/routeNames';
 
 const Settings = () => {
   const {authDispatch} = useContext(GlobalContext);
+  const navigate = useNavigation();
 
   const handleLogout = () => {
     Alert.alert('Logout!', 'Are you sure you want to logout? ', [
@@ -17,7 +20,9 @@ const Settings = () => {
       {
         text: 'OK',
         onPress: () => {
-          logoutAction()(authDispatch);
+          logoutAction()(authDispatch)(() => {
+            navigate.navigate(LOGIN);
+          });
         },
       },
     ]);
